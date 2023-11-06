9b1f703f0d5bc23d7b00dc6f2041be4b906afc5a
Rajendra Kumar <kurajendra@altimetrik.com>
2023-11-07 00:48:36 +0530

    kafka base service commit

---
+++ src/test/java/com/base/baseservice/BaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.baseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
